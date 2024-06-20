6edb14f903539b1fe844b74da4e634f75cda2e6a
shonburgundy <seanbeloussow@yahoo.com>
2024-06-19 19:27:32 -0700

    chore: update config to not run webkit as it does NOT work on my machine, extend test timeout as my machine is old and slow

--- playwright.config.js
+++ playwright.config.js
@@ -7,11 +7,13 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 // require('dotenv').config();
 
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
   testDir: './tests',
+  timeout: 60 * 1000, // 60 seconds
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -43,10 +45,10 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
